99d536e6ff88397665badef014ab405bb8856c5f
Matthew <wispmatvei@gmail.com>
2024-06-09 18:35:13 +0300

    Catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BaseButton from "./modules/components/BaseButton";
+import { ReactComponent as Cart } from "./assets/Cart.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" style={{ marginTop: 100 }}>
+            <BaseButton icon={<Cart />} counter={0} />
+        </div>
+    );
 }
 
 export default App;
